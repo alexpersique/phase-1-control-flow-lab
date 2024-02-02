5d0cf4a59fc1d9f885651496bfb453844127e601
Alexander <alexdgenkin@gmail.com>
2024-02-02 19:47:11 +1100

    Fills index.js

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
-}
+  let result 
+  
+  if (someValue <= 400) 
+    {
+      result = 'This one is on me!' 
+    
+    } else if (someValue > 400 && someValue <= 2000)
+      { 
+        result = 'That will be twenty bucks.'
+    } else if (someValue >2000 && someValue <= 2500)
+        { 
+        result = 'I will gladly take your thirty bucks.'
+    } else if (someValue >2500)
+      {
+        result = 'No can do.'
+      }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+    return result
+   
+    
 
-function switchOnCharmFromTip(){
+  }
+
+  
+
+function ternaryCheckCity(city){
   // Write your code here!
+  
+
+return city === 'NYC' ? "Ok, sounds good." : "No go.";
+
+
+
+
+
+
 }
+  
+
+  
+
+  
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+     return "Thank you so much.";
+    case 'not as generous':
+     return "Thank you.";
+     break
+    default: 
+     return "Bye.";
+    
+  }
+   
+  
+  }
+
+
